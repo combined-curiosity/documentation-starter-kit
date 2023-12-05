6f26fc434eabcdaf2d7602ce5b2c967669322a98
willisjordancc <97904419+willisjordancc@users.noreply.github.com>
2023-12-05 16:32:22 -0500

    more config

--- theme.config.tsx
+++ theme.config.tsx
@@ -17,35 +17,41 @@ const config: DocsThemeConfig = {
       }
     }
   },
-  logo: <Logo />,
-  logoLink: "/",
-  project: {
-    link: 'https://github.com/willisjordancc/documentation-starter-kit',
+  banner: {
+    "dismissible": true,
+    "key": "notice-banner",
+    "text": "Brand spanking new Doc Site. Enjoy!",
   },
   chat: {
     link: 'https://combinedcuriosity.slack.com/archives/C042JJE86AH',
     icon: <FaSlack style={{ width: "25px", height: "25px" }} />,
   },
   docsRepositoryBase: 'https://github.com/willisjordancc/documentation-starter-kit/blob/main',
-  footer: {
-    component: <span/>,
-  },
-  banner: {
-    "dismissible": true,
-    "key": "notice-banner",
-    "text": "Brand spanking new Doc Site. Enjoy!",
-  },
-  toc: {
-    backToTop: true,
-  },
   feedback: {
     useLink: () => "https://combinedcuriosity.slack.com/archives/C042JJE86AH",
+  },
+  footer: {
+    component: <span/>,
   },
   head: (
     <>
       <meta name="favicon" content="/favicon.ico" />
     </>
   ),
+  logo: <Logo />,
+  logoLink: "/",
+  primaryHue: { dark: 480, light: 650 },
+  primarySaturation: { dark: 70, light: 100 },
+  project: {
+    link: 'https://github.com/willisjordancc/documentation-starter-kit',
+  },
+  sidebar: {
+    autoCollapse: true,
+    defaultMenuCollapseLevel: 1,
+  }, 
+  toc: {
+    backToTop: true,
+  },
 }
 
 export default config
